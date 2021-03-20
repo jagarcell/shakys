63ea8e8337c9220bf8df8e7abb2bc4892f1071ef
Jorge Garcell <jagarcell@gmail.com>
2021-03-20 17:24:21 +0000

    Welcome Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,21 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-wf-page="605538017156323a2f5ab124" data-wf-site="604d41d40c813292693d08e7">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Admin Panel</title>
+        <meta content="admin panel" property="og:title">
+        <meta content="admin panel" property="twitter:title">
+        <meta content="width=device-width, initial-scale=1" name="viewport">
+        <meta content="Webflow" name="generator">
+        <link href="css/normalize.css" rel="stylesheet" type="text/css">
+        <link href="css/webflow.css" rel="stylesheet" type="text/css">
+        <link href="css/shakys.webflow.css" rel="stylesheet" type="text/css">
+        <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
+        <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
+        <link href="images/favicon.ico" rel="shortcut icon" type="image/x-icon">
+        <link href="images/webclip.png" rel="apple-touch-icon">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -19,6 +30,7 @@
                 font-family: 'Nunito', sans-serif;
             }
         </style>
+      <meta name="format-detection" content="telephone=no">
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
@@ -128,5 +140,41 @@
                 </div>
             </div>
         </div>
+        <div class="page-header">
+        <div class="logo-frame"><img src="images/Shakys.png" loading="lazy" sizes="(max-width: 767px) 100vw, 53vw" srcset="images/Shakys-p-500.png 500w, images/Shakys.png 512w" alt="" class="logo"></div>
+        <div class="title-frame">
+        <div class="text-block">PURCHASES CONTROL SYSTEM</div>
+        </div>
+    </div>
+    <div class="page_title_frame">
+        <div class="page_title">ADMIN PANEL</div>
+    </div>
+    <div class="admin_panel_section">
+        <div class="option_frame"><img src="images/pendingorders.png" loading="lazy" sizes="150px" width="150" srcset="images/pendingorders.png 500w, images/pendingorders.png 512w" alt="" class="option_image">
+        <div class="option_text">PENDING ORDERS</div>
+        </div>
+        <div class="option_frame"><img src="images/products.png" loading="lazy" width="150" sizes="150px" srcset="images/products.png 500w, images/products.png 600w" alt="" class="option_image">
+        <div class="option_text">PRODUCTS </div>
+        </div>
+    </div>
+    <div class="admin_panel_section">
+        <div class="option_frame"><img src="images/productlocation.png" loading="lazy" sizes="150px" width="150" srcset="images/productlocation.png 500w, images/productlocation.png 512w" alt="" class="option_image">
+        <div class="option_text">IN-STORE PRODUCT LOCATIONS</div>
+        </div>
+        <div class="option_frame"><img src="images/supplierproductlocation.png" loading="lazy" width="150" sizes="150px" srcset="images/supplierproductlocation.png 500w, images/supplierproductlocation.png 512w" alt="" class="option_image">
+        <div class="option_text">PRODUCT LOCATIONS<br> AT SUPPLIERS</div>
+        </div>
+    </div>
+    <div class="admin_panel_section">
+        <div class="option_frame"><img src="images/Users-icon.png" loading="lazy" width="150" alt="" class="option_image">
+        <div class="option_text">USERS</div>
+        </div>
+        <div class="option_frame"><img src="images/Suppliers.png" loading="lazy" sizes="150px" width="150" srcset="images/Suppliers.png 500w, images/Suppliers.png 512w" alt="" class="option_image">
+        <div class="option_text">SUPPLIERS</div>
+        </div>
+    </div>
+    <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=604d41d40c813292693d08e7" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="js/webflow.js" type="text/javascript"></script>
+    <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
     </body>
 </html>
