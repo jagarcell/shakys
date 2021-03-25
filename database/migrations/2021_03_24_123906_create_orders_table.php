4e7780b5e176a106c085f641069b329a230b8966
Jorge Garcell <jagarcell@gmail.com>
2021-03-25 12:38:16 +0000

    orders migrations

---
+++ database/migrations/2021_03_24_123906_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('supplier_id')->default(-1);
+            $table->dateTime('date');
+            $table->boolean('pickup')->default(true);
+            $table->bigInteger('pickup_guy_id')->default(-1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
---
+++ database/migrations/2021_03_24_123916_create_order_lines_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderLinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_lines', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('order_id')->default(-1);
+            $table->bigInteger('product_id')->default(-1);
+            $table->double('qty')->default(0);
+            $table->double('purchased_qty')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_lines');
+    }
+}
