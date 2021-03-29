038e8fa762f5102d4942ef53faeb8c9c275e85da
Jorge Garcell <jagarcell@gmail.com>
2021-03-29 16:47:57 +0000

    c

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        $section('title', 'Admin Panel')
+        @section('title', 'Admin Panel')
 
         @section('headsection')
             @parent
