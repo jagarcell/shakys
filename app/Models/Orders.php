e09cbc45b58a2932be4bb8f7f180e2e1d1d35e57
Jorge Garcell <jagarcell@gmail.com>
2021-06-14 11:41:26 +0000

    c

--- app/Models/Orders.php
+++ app/Models/Orders.php
@@ -267,7 +267,7 @@ class Orders extends Model
         try {
             //code...
             if(strlen($Order->email) > 0){
-                Mail::to($Order->email)->send((new OrderEmail($Order))->subject($Subject));
+//                Mail::to($Order->email)->send((new OrderEmail($Order))->subject($Subject));
             }
             else{
                 return ['status' => 'noemail', 'element_tag' => $ElementTag];
