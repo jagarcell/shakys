625a43c621e6ebedacb39c60c71260d802db42e3
Jorge Garcell <jagarcell@gmail.com>
2021-09-10 12:46:19 +0000

    c

--- app/Http/Middleware/CheckUsersState.php
+++ app/Http/Middleware/CheckUsersState.php
@@ -49,7 +49,7 @@ class CheckUsersState
             try {
                 //code...
                 if((new Users())->HasUsers()){
-                    return redirect('/login');
+                    return redirect('/login1');
                 }
                 else{
                     return redirect('/register');
