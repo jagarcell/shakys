6a28ad89db9d3d4eb7f697134df822d04b0e2cda
Jorge Garcell <jagarcell@gmail.com>
2022-04-21 18:10:55 -0400

    cc

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -117,9 +117,12 @@
                     </li>    
                     <li class="nav-item dropdown" style="text-decoration:underline;">
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('login') }}"
+                            <!--a class="dropdown-item" href="{{ route('logout') }}"
                                 onclick="event.preventDefault();
                                                 document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a-->
+                            <a class="dropdown-item" href="{{ route('login') }}">
                                 {{ __('Logout') }}
                             </a>
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
