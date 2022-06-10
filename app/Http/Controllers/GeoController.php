5980d7fa96a4986e975c7ebaf1d0fcb96a874710
jorge-digitalyalo <104593507+jorge-digitalyalo@users.noreply.github.com>
2022-06-10 13:52:34 -0400

    c

--- app/Http/Controllers/GeoController.php
+++ app/Http/Controllers/GeoController.php
@@ -17,6 +17,9 @@ class GeoController extends Controller
     public function getClientIp(Request $request)
     {
         $url = 'http://ip-api.com/json/96.225.102.144';
+        $curl = curl_init($url);
+        return curl_exec($curl);
+
         $tz = file_get_contents($url);
         $tz = json_decode($tz,true)['timezone'];
         return $tz;
