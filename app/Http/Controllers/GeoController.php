b5818e6ff90e8b1510201761f74829cb963718d4
jorge-digitalyalo <104593507+jorge-digitalyalo@users.noreply.github.com>
2022-06-10 14:08:05 -0400

    c

--- app/Http/Controllers/GeoController.php
+++ app/Http/Controllers/GeoController.php
@@ -19,9 +19,15 @@ class GeoController extends Controller
         $url = 'http://ipecho.net/plain';
         $curl = curl_init($url);
         curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);        
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+         
+        //for debug only!
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
         $resp = curl_exec($curl);
         curl_close($curl);
+        var_dump($resp);
         return $resp;
         $tz = file_get_contents($url);
         $tz = json_decode($tz,true)['timezone'];
